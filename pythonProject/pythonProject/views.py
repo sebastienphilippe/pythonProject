f1a849e381a4f641ef89281b359cfe5c05505247
Thomasthiawthi <thomas.thiaw-thi@hetic.net>
2020-01-27 11:15:03 +0100

    test
    views change

--- pythonProject/pythonProject/views.py
+++ pythonProject/pythonProject/views.py
@@ -10,4 +10,9 @@ def projet(request):
     return HttpResponse("""
         <h1>Hola que tal</h1>
         <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
+        <form>
+            <label for="name">Enter your name: </label>
+            <input type="text" name="name" id="name" required>
+        </form>
+
     """)
