b5a7a75a5145bfcab16c0312b25afde79cbaa3c9
sebastienphilippe <45207662+sebastienphilippe@users.noreply.github.com>
2020-01-28 10:07:25 +0100

    Create views.py

---
+++ pythonProject/pythonApp/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def project(request):
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return HttpResponse("""
+        <h1>Hola que tal</h1>
+        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
+        <form>
+            <label for="name">Enter your name: </label>
+            <input type="text" name="name" id="name" required>
+        </form>
+
+    """)
